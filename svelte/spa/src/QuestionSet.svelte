1189ba691920d492cdc1e3d58c492592f38950a3
mtempleheald@hotmail.com <mtempleheald@hotmail.com>
2020-11-08 11:01:03 +0000

    move data management out of the store

--- svelte/spa/src/QuestionSet.svelte
+++ svelte/spa/src/QuestionSet.svelte
@@ -1,6 +1,24 @@
 <script>
 	import TextQuestion from './TextQuestion.svelte';
 	import { questionSet } from './stores';
+	const qs = [
+    {
+      "id" : 1,
+      "label": "Question 1",
+      "type": "TextQuestion",
+      "placeholder": "Placeholder...",
+      "help": "Help text",
+      "pre": "Markdown before question",
+      "post": "Markdown after question"
+    },
+    {
+      "id" : 2,
+      "label": "Question 2",
+      "type": "TextQuestion"
+    }
+	];
+	// TODO - pull this from an external API (Back office tooling)
+	questionSet.set(qs);
 </script>
 
 
--- svelte/spa/src/stores.js
+++ svelte/spa/src/stores.js
@@ -1,31 +1,6 @@
 import { writable } from 'svelte/store';
 
-function createQuestionSet() {
-
-  const defaultQuestionSet = [
-    {
-      "id" : 1,
-      "label": "Question 1",
-      "type": "TextQuestion",
-      "placeholder": "Placeholder...",
-      "help": "Help text",
-      "pre": "Markdown before question",
-      "post": "Markdown after question"
-    },
-    {
-      "id" : 2,
-      "label": "Question 2",
-      "type": "TextQuestion"
-    }
-  ];
-
-  const { subscribe, set } = writable(defaultQuestionSet);
-
-  return {
-    subscribe,
-    set: () => set(defaultQuestionSet)
-  }
-}
-
-export const questionSet = createQuestionSet();
+// questionSet designed to be overwritten in full, hence no add/remove functions
+// this is for an input form for which the IDs need to be known for later submission
+export const questionSet = writable([]);
 
